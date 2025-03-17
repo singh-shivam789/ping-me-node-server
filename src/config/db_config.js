486dc1cdad615c3849ad163d4a85bf6e07fcd6b2
singh-shivam789 <shivamsinghgbpit@gmail.com>
2025-03-18 01:43:38 +0530

    Enhance error handling for db_config

--- src/config/db_config.js
+++ src/config/db_config.js
@@ -1,27 +1,27 @@
 
 import { MongoClient, ServerApiVersion } from "mongodb";
+import { defaultLogger, errorLogger } from "../utils/LoggerUtils.js";
 import dotenv from "dotenv"
 dotenv.config();
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@dev-mongo-cluster-1.fc5mj.mongodb.net/?retryWrites=true&w=majority&appName=dev-mongo-cluster-1?directConnection=true`;
-
-const client = new MongoClient(uri, {
-    serverApi: {
-        version: ServerApiVersion.v1,
-        strict: true,
-        deprecationErrors: true,
-    }
-});
-
+let client;
 try {
+    client = new MongoClient(uri, {
+        serverApi: {
+            version: ServerApiVersion.v1,
+            strict: true,
+            deprecationErrors: true,
+        }
+    });
     client.connect().then(() => {
-        console.log("Connected to Mongo");
+        defaultLogger("info", "Successfully established connection to Mongo Client");
     }).catch(err => {
         throw err;
     }).finally(
         await client.close()
     )
 } catch (error) {
-    console.log(error);
+    errorLogger("error", error.message);
 }
 
 export default client;
