644d1d89c86222123bf39e381b91e17101704138
singh-shivam789 <shivamsinghgbpit@gmail.com>
2025-03-18 21:49:00 +0530

    Implement Authorizer middleware

---
+++ src/utils/middlewares/auth.js
@@ -0,0 +1,18 @@
+import jsonwebtoken from "jsonwebtoken";
+import dotenv from "dotenv";
+import { errorLogger, defaultLogger } from "../LoggerUtils.js";
+dotenv.config();
+
+const JWT_SECRET = process.env.JWT_SECRET;
+export function authorizer(req, res, next) {
+    try {
+        jsonwebtoken.verify(req.cookies.token, JWT_SECRET);
+        next();
+    } catch (error) {
+        errorLogger("error", error.message);
+        return res.status(401).json({
+            code: 401, 
+            message: "Session expired or invalid token, please sign in again"
+        });
+    }
+}
